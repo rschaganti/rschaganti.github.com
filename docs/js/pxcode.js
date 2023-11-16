2f4b45089c90d449e9c145a5d385e4b51f8a8a08
rschaganti <rschaganti@yahoo.com>
2023-11-16 23:24:46 +0530

    Update pxcode.js

--- docs/js/pxcode.js
+++ docs/js/pxcode.js
@@ -4,3 +4,22 @@
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-HCLEKDYC6QID-2");
 
+//passing user and account objects:
+aptrinsic("identify",
+  {
+  //User Fields
+    "id": "unique-user-id", // Required for logged in app users
+    "email": "userEmail@address.com",
+    "firstName": "John",
+    "lastName": "Smith",
+    "signUpDate": 1522697426479, //unix time in ms
+    "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+    "price" : 95.5,
+    "userHash": "" // optional transient for HMAC identification
+  },
+  {
+  //Account Fields
+    "id":"IBM", //Required
+    "name":"International Business Machine",
+    "Program": "Platinum" // flat custom attributes
+ });
